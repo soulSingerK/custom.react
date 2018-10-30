47fa1ecaa9b054d9f8f552f5d96bf75714f6e2d1
Gshisan <425139963@qq.com>
2018-10-30 14:35:16 +0800

    feat: react react-dom base feature

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from './src/react'
+import ReactDOM from './src/react-dom'
+
+const element = (
+  <div>
+    hello <span>you zhege da zhu tizi</span>
+    <h2>12</h2>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
